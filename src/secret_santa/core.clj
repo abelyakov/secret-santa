73a03082459ce10981e96ed83e935c9897cc0fc6
Andrej.Belyakov <Andrej.Belyakov@tstn.ru>
2014-12-11 00:39:05 +0300

    email sending works

--- src/secret_santa/core.clj
+++ src/secret_santa/core.clj
@@ -9,7 +9,18 @@
             [clojure.data.json :as json]
             [clojure.pprint :as pprint]
             [secret-santa.rand :as rand]
-            ))
+            [postal.core :as postal]))
+
+(defn send-email [to text]
+  (postal/send-message {:host "smtp.gmail.com"
+                        :user ""
+                        :pass ""
+                        :ssl true
+                        }
+                       {:from "lstghost@gmail.com "
+                        :to to
+                        :subject "С наступающим!"
+                        :body (str "Новогоднее жюри решило, что ты даришь подарок человеку с ящиком li" text)}))
 
 (def emails (atom #{}))
 
@@ -25,12 +36,16 @@
       (= 1 (count email-list)) {(first email-list) (first email-list)}
       :else (rand/gen-pairs email-list))))
 
+(defn send-emails [emails]
+  (map #(send-email (key %) (val %)) emails))
+
 
 (defroutes compojure-handler
   (POST "/send-email" [email] (save-email email))
   (GET "/" [] (redirect "/index.html"))
   (GET "/list" [] (json/write-str @emails))
   (GET "/rand" [] (json/write-str (start-draw)))
+  (GET "/mail" [] (send-emails (start-draw)))
   (route/resources "/")
   (route/not-found "<h1>Page not found!</h1>"))
 
