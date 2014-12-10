2a7d6705602d0404d1f37ead823108394794ebbf
Andrej.Belyakov <Andrej.Belyakov@tstn.ru>
2014-12-10 13:31:33 +0300

    server starts, form gets email

--- src/secret_santa/core.clj
+++ src/secret_santa/core.clj
@@ -1,16 +1,28 @@
 (ns secret-santa.core
 	(:require [ring.adapter.jetty :refer :all]
-			  [ring.util.response :refer :all]
-			  [compojure.core :refer :all]
-		      [compojure.route :as route]))
+                  [ring.util.response :refer :all]
+                  [ring.middleware.content-type :refer :all]
+                  [ring.middleware.params :refer [wrap-params]]
+                  [compojure.core :refer :all]
+                  [compojure.route :as route]
+                  [compojure.handler :refer [api site]]))
 
-; (defroutes app GET "/")
+(def emails (atom #{}))
 
-(defn handler [request]
-	(response (str request)))
+(defn process-email-post [email]
+  (swap! emails conj email)
+  "Спасибо за участие!")
 
-; (defn- main[]
-; 	(run-jetty handler {:port 3000 :join? false}))
- 
+(defroutes compojure-handler
+  (POST "/send-email" [email] (process-email-post email))
+  (GET "/" [] (redirect "/index.html"))
+  (GET "/list" [] (str @emails))
+  (route/resources "/")
+  (route/not-found "<h1>Page not found!</h1>"))
 
-(def app handler)
+(def app 
+	( -> compojure-handler
+             wrap-params))
+
+
+
